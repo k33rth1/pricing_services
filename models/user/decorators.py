f1620302b2433f39def28e9dfccd3787aec60c44
k33rth1 <k33rth1k4nch@gmail.com>
2020-07-25 09:23:08 +0530

    made changes

--- models/user/decorators.py
+++ models/user/decorators.py
@@ -4,7 +4,7 @@ from flask import session, flash, redirect, url_for,current_app
 
 def requires_login(f: Callable) -> Callable:
     @functools.wraps(f)
-    def decorated_function(*args, **kwargs):
+    def decorated_function(*args,**kwargs):
         if not session.get('email'):
             flash('You need to be signed in for this page', 'danger')
             return redirect(url_for('users.login_user'))
@@ -13,7 +13,7 @@ def requires_login(f: Callable) -> Callable:
 
 def requires_admin(f:Callable) -> Callable:
     @functools.wraps(f)
-    def decorated_function(*args, **kwargs):
+    def decorated_function(*args,**kwargs):
         if session.get('email') != current_app.config.get('ADMIN',''):
             flash('You need to be an administrator to access this page.', 'danger')
             return redirect(url_for('users.login_user'))
